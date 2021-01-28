7e826be4a33ae47889624d87b646a16ccf880d69
burju <kanliska@gmail.com>
2021-01-27 19:36:04 -0600

    a new solar system added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
 
+    public static void main(String[] args) {
 
+        SolarSystem solarSystem01= new SolarSystem();
+        solarSystem01.sun= 1;
+        solarSystem01.moon=2;
+        solarSystem01.planets=3;
+        
+    }
 
 }
