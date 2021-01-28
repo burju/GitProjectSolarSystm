74e25ecd1b3fc42c090b7009d469a678c2ca9289
burju <kanliska@gmail.com>
2021-01-27 19:51:06 -0600

    new sun features added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import com.company.SUN;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -6,6 +8,11 @@ public class Main {
         solarSystem01.sun= 1;
         solarSystem01.moon=2;
         solarSystem01.planets=3;
+
+        SUN troyo= new SUN();
+        troyo.heat= "12344323";
+        troyo.radius= "12343234";
+        troyo.temperature= 123244243;
         
     }
 
---
+++ src/com/company/SUN.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class SUN {
+
+    public int temperature;
+    public String heat;
+    public  String radius;
+
+}
